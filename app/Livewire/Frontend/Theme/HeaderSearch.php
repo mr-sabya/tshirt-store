bdbe665d8b139d5b368bd615a0bf5c8fd2e110d3
mr-sabya <sabya.citik@gmail.com>
2025-03-24 12:56:23 +0600

    search products

--- app/Livewire/Frontend/Theme/HeaderSearch.php
+++ app/Livewire/Frontend/Theme/HeaderSearch.php
@@ -2,10 +2,21 @@
 
 namespace App\Livewire\Frontend\Theme;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class HeaderSearch extends Component
 {
+    public $query = '';  // Property to hold the search query
+    public $products = [];  // Property to hold the search results
+
+    // Method to update search results when query changes
+    public function updatedQuery()
+    {
+        $this->products = Product::where('name', 'LIKE', "%{$this->query}%")
+            ->get();
+    }
+
     public function render()
     {
         return view('livewire.frontend.theme.header-search');
--- resources/views/livewire/frontend/theme/header-search.blade.php
+++ resources/views/livewire/frontend/theme/header-search.blade.php
@@ -1,7 +1,35 @@
 <div class="header-search">
-    <form class="ec-btn-group-form" action="#">
-        <input class="form-control ec-search-bar" placeholder="Search products..."
-            type="text">
+    <form wire:submit.prevent="updatedQuery">
+        <input class="form-control ec-search-bar" type="text" wire:model.live="query" placeholder="Search products..." />
         <button class="submit" type="submit"><i class="fi-rr-search"></i></button>
     </form>
+
+    @if($query)
+    <div class="search-results">
+        @if($products->count() > 0)
+        <div class="row">
+            @foreach($products as $product)
+            <div class="col-lg-12">
+                <a href="{{ route('product.show', $product->slug) }}" wire:navigate class="w-100">
+                    <div class="product-card">
+                        <img src="{{ url('storage/'.$product->image) }}" alt="{{ $product->name }}">
+                        <div class="text">
+                            <h5 class="m-0">{{ $product->name }}</h5>
+                            <p class="m-0">{{ \Illuminate\Support\Str::limit($product->short_desc, 50) }}</p>
+                            <p class="m-0">৳ {{ number_format($product->price, 2) }}</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            @endforeach
+        </div>
+        @else
+        <p>No products found for your search.</p>
+        @endif
+
+        <div class="mt-2">
+            <a class="show-all" href="{{ route('shop.index') }}" wire:navigate>Show All Products</a>
+        </div>
+    </div>
+    @endif
 </div>
